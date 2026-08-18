42d36c5f8d5e7360567e40fdcbd4af586aeffd86
Goutham-VR <gouthamretheesh143@gmail.com>
2026-08-19 03:42:26 +0530

    REST CURD Completed

--- Phase 1/Projects/DjangoREST/myrestprojectone/api/urls.py
+++ Phase 1/Projects/DjangoREST/myrestprojectone/api/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from api import views
 
 urlpatterns=[
+    #Basic API Requests
     path('hello/',views.hello), #GET retrieve data.
     path('student/',views.student), #POST send json data to the server.
     path('officer/',views.officer), #POST send json data to the server and print each data.
     path('getpost/',views.getpost), #Let's make one API that accepts both GET and POST.
+    path('customfun/',views.customfun), #GET with some async method.
+
+    #CURD Operation
+    path('create/',views.create), #POST Create/Insert to table
+    path('getdata/<int:id>/',views.getdata), #GET Select data from table
+    path('deletedata/<int:id>/',views.deletedata), #DELETE delete data from db using PK
+    path('updatedata/<int:id>/',views.updatedata), #PUT update data from db using PK
 ]
--- Phase 1/Projects/DjangoREST/myrestprojectone/api/views.py
+++ Phase 1/Projects/DjangoREST/myrestprojectone/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from api.models import Student
+
+import asyncio
 # Create your views here.
 
 @api_view(["GET"])
@@ -38,3 +41,61 @@ def getpost(request):
             'message':"POST Request",
             'data':request.data
         })
+
+@api_view(['GET'])
+def customfun(request):
+    async def task1():
+        print("Task 1 Start")
+        await asyncio.sleep(5)
+        print("Task 1 End")
+    async def task2():
+        print("Task 2 Start")
+        await asyncio.sleep(2)
+        print("Task 2 End")
+    async def main():
+        await asyncio.gather(task1(),task2())
+
+    asyncio.run(main())
+    return Response({
+        'message':"Task Finished"
+    })
+
+@api_view(['POST'])
+def create(request):
+    Student.objects.create(name=request.data['name'],
+                           age=request.data['age'],
+                           course=request.data['course'])
+    return Response({
+        'message':'Data Inserted'
+    })
+
+@api_view(['GET'])
+def getdata(request,id):
+    dbdata=Student.objects.get(id=id)
+    Name=dbdata.name
+    Age=dbdata.age
+    Course=dbdata.course
+    return Response({
+        'message':'Data Retrieved',
+        'Name':Name,
+        'Age':Age,
+        'Course':Course
+    })
+
+@api_view(['DELETE'])
+def deletedata(request,id):
+    Student.objects.get(id=id).delete()
+    return Response({
+        'message':"Data Deleted"
+    })
+
+@api_view(['PUT'])
+def updatedata(request,id):
+    dbdata=Student.objects.get(id=id)
+    dbdata.name=request.data['name']
+    dbdata.age=request.data['age']
+    dbdata.course=request.data['course']
+    dbdata.save()
+    return Response({
+            'message':"Data Updated"
+    })
